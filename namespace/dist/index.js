53633f53cb05cc1c5dad7f22c6eef6234a00d9b3
DaTongDXM <953909305@qq.com>
2020-10-18 21:02:09 +0800

    1008

---
+++ namespace/dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var AllAnimal;
+(function (AllAnimal) {
+    var Dog = /** @class */ (function () {
+        function Dog() {
+            console.log('this is a dog');
+        }
+        return Dog;
+    }());
+    var Cat = /** @class */ (function () {
+        function Cat() {
+            console.log('this is a cat');
+        }
+        return Cat;
+    }());
+    var Bird = /** @class */ (function () {
+        function Bird() {
+            console.log('this is a bird');
+        }
+        return Bird;
+    }());
+    var Animal = /** @class */ (function () {
+        function Animal() {
+            new Dog();
+            new Cat();
+            new Bird();
+        }
+        return Animal;
+    }());
+    AllAnimal.Animal = Animal;
+})(AllAnimal || (AllAnimal = {}));
---
+++ namespace/src/index.ts
@@ -0,0 +1,28 @@
+namespace AllAnimal{
+    class Dog{
+        constructor(){
+            console.log('this is a dog');
+            
+        }
+    }
+    class Cat{
+        constructor(){
+            console.log('this is a cat')
+        }
+    }
+    class Bird{
+        constructor(){
+            console.log('this is a bird')
+        }
+    }
+    
+    export class Animal{
+        constructor(){
+            new Dog();
+            new Cat();
+            new Bird();
+        }
+    }
+}
+
+
